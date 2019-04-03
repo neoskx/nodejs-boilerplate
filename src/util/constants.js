20ab9a7801615c72de57e8d1944e6989b7328b6e
Shaoke <shaoke.xu@oracle.com>
2019-04-03 15:13:15 -0700

    Get service name from package.json

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,10 +1,12 @@
+const packageJson = require('../../package.json');
+
 const CONFIG = {
     LOG_FILES_PATH: './public/log',
     NODE_ENV: 'development',
-    SERVICE_NAME: 'nodejs-boilerplate',
+    SERVICE_NAME: packageJson.name,
     LOG_LEVEL: 'debug',
     PORT: 9099,     // server port number
-    MONGODB_URI: 'mongodb://localhost:27017/node-boilerplate'
+    MONGODB_URI: `mongodb://localhost:27017/${packageJson.name}`
 };
 
 const COLLECTIONS_NAME = {
